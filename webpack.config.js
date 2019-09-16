3a5c9177be89cd97a44e25b450906114e1724d52
Andy Wong <andywong.ny@gmail.com>
2019-09-16 17:35:45 -0400

    slimmed down config file for context-api

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,11 @@ module.exports = {
           options: {
             presets: [
               '@babel/preset-env',
-              {
-                plugins: [
-                  '@babel/plugin-proposal-class-properties'
-                ]
-              }
+              // {
+              //   plugins: [
+              //     '@babel/plugin-proposal-class-properties'
+              //   ]
+              // }
             ]
           },
         }
