acadc3a3697207d85071a152d6eb07f7cec6e792
Andy Wong <andywong.ny@gmail.com>
2019-09-16 16:10:17 -0400

    first commit

---
+++ src/ContextApi.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+const initialState = {
+  state: {
+    name: "No One",
+    age: 0
+  }
+}
+
+const MyContext = React.createContext(initialState);
+
+class App extends Component {
+  state = {
+    name: "Andy",
+    age: 27
+  }
+  render() {
+    return (
+      <MyContext.Provider value={
+        {
+          state: this.state,
+          itsMyBirthday: () => {
+            this.setState({
+              age: this.state.age + 1
+            })
+            console.log(this.state);
+          }
+        }
+      }>
+        <Family />
+      </MyContext.Provider>
+    );
+  }
+}
+
+function Family(props) {
+  return (
+    <div>
+      <Person />
+    </div>
+  )
+}
+
+class Person extends Component {
+
+  static contextType = MyContext;
+
+  render() {
+    return (
+      <div className="person">
+        <MyContext.Consumer>
+          {(context) => (
+            <React.Fragment>
+              <p>React Context API Demo</p>
+              <div>{"Name: " + context.state.name}</div>
+              <div>{"Age: " + context.state.age}</div>
+              <button onClick={context.itsMyBirthday}>Get Older</button>
+            </React.Fragment>
+
+          )}
+        </MyContext.Consumer>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import UseStateHook from './UseStateHook';
+import ContextApi from './ContextApi';
+import reactime from '../../reactime/package/index'
+
+const root = document.getElementById('root');
+
+// ReactDOM.render(<UseStateHook />, root);
+ReactDOM.render(<ContextApi />, root);
+
+reactime(root);
